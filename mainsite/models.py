0f00a4106cb8457563ac99230e672efa3d1e7b7d
Lanting Chiang <lantingchiang@gmail.com>
2020-05-22 00:12:07 -0400

    update models to include image field

--- mainsite/models.py
+++ mainsite/models.py
@@ -1,18 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
 
 
 class Store(models.Model):
-    # TODO - figure out smart way to store image
     store_name = models.CharField(max_length=200, unique=True)
     location = models.TextField()
     description = models.TextField()
+    store_img = models.ImageField(upload_to=store_name, null=True)
 
     def __str__(self):
         return self.store_name
 
 
 class Hashtag(models.Model):
-    # TODO - catch error when unique constraint is violated
     tag = models.CharField(max_length=200, unique=True)
 
     def __str__(self):
@@ -21,19 +22,23 @@ class Hashtag(models.Model):
 
 class Look(models.Model):
     # look gets deleted when store is deleted
-    store = models.ForeignKey(Hashtag, on_delete=models.CASCADE)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Hashtag)
+
     likes = models.IntegerField(default=0)
-    # TODO - image field
+    image = models.ImageField(upload_to=store.store_name, null=True)
+    location = models.TextField()
 
 
 class Product(models.Model):
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    look = models.OneToOneField(Look)
+
     item_name = models.CharField(max_length=200, unique=True)
-    store = models.ForeignKey(Hashtag, on_delete=models.CASCADE)
-    tags = models.ManyToManyField(Hashtag)
-    price = models.DecimalField(max_digits=7, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     details = models.TextField()
     stock = models.IntegerField(default=0)
+    image = models.ImageField(upload_to=store.store_name, null=True)
 
     def __str__(self):
         return self.item_name
--- mainsite/views.py
+++ mainsite/views.py
@@ -1,7 +1,46 @@
 from django.http import HttpResponse
+from django.views.generic import ListView
 from django.views import View
 
 
 class HomeView(View):
     def get(self, request):
-        return HttpResponse("this will be the home page")
+        return HttpResponse("This will be the home page")
+
+
+class FeedView(View):
+    # https://docs.djangoproject.com/en/3.0/ref/contrib/syndication/
+
+    def get(self, request):
+        return HttpResponse(
+            "This page should dynamically render looks \
+                             based on followed tags, gender and location "
+        )
+
+
+class ProfileView(ListView):
+    """
+    Sends db entry to front end. Only get requests allowed
+    """
+
+    # model =
+    # template_name =
+    pass
+
+
+class HashtagView(View):
+    def get(self, request):
+        """
+        Front end will likely need to pass the hashtag name in some request field or in url.
+        https://docs.djangoproject.com/en/3.0/topics/class-based-views/generic-display/#dynamic-filtering
+        """
+        return HttpResponse("Renders all looks labeled by this hashtag in chronological order")
+
+
+class ProductView(View):
+    """
+    Renders information about product. Will likely link to SHOPPING CART.
+    """
+
+    def get(self, request):
+        return HttpResponse("Product page")
