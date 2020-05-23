f0b2d8d40ebf3a43a2cc20e3b3ac25c9173caabd
Lanting Chiang <lantingchiang@gmail.com>
2020-05-23 06:39:15 -0400

    remove node_modules, fix urls, add djangoREST to pipfile

--- Lookbook/urls.py
+++ Lookbook/urls.py
@@ -18,7 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path("", include("mainsite.urls")),
-    path('', include('frontend.urls')),
-    path('api/', include('todos.api.urls')),
+    path("api/", include("mainsite.urls")),
+    path("", include("frontend.urls")),
+    # we won't need the todo app after making frontend the frontend of mainsite
+    path("todo/api", include("todos.api.urls")),
 ]
