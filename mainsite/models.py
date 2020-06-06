ab899daafae87eef1ed7c14f8520efa5b247fe49
Lanting Chiang <lantingchiang@gmail.com>
2020-06-06 02:29:29 -0400

    update models & serializer

--- mainsite/models.py
+++ mainsite/models.py
@@ -58,24 +58,8 @@ class Store(models.Model):
         return self.store_name
 
 
-class Look(models.Model):
-    # look gets deleted when store is deleted
-    store = models.ForeignKey(Store, on_delete=models.CASCADE)
-    tags = models.ManyToManyField(Hashtag)
-
-    likes = models.IntegerField(default=0)
-    # TODO - add upload_to parameter to upload to specific folder
-    image = models.ImageField(null=True)
-    location = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        ordering = ["-created_at"]
-
-
 class Product(models.Model):
     store = models.ForeignKey(Store, on_delete=models.CASCADE)
-    look = models.OneToOneField(Look, on_delete=models.DO_NOTHING)
 
     item_name = models.CharField(max_length=200, unique=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -88,6 +72,19 @@ class Product(models.Model):
 
     def __str__(self):
         return self.item_name
+
+
+class Look(models.Model):
+    tags = models.ManyToManyField(Hashtag)
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+
+    likes = models.IntegerField(default=0)
+    # TODO - add upload_to parameter to upload to specific folder
+    image = models.ImageField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
 
 
 class ProductImage(models.Model):
